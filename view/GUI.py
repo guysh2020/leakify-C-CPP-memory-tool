a361e5114d7808764d49d6c1c2cc54ed33365e6a
sivansalzmann <sivansalzmann10@gmail.com>
2021-01-12 09:43:39 +0200

    start GUI

--- view/GUI.py
+++ view/GUI.py
@@ -1,37 +1,68 @@
-# from View import  *
-import sys
-import app as app
-from PyQt5.QtWidgets import *
+import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import Menu
+from tkinter import filedialog
 
-class GUI(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
+class App:
+    def __init__(self, root):
+        #setting title
+        root.title("Leakify")
+        #setting window size
+        width=800
+        height=500
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+        #Exit action
+        def _quit():
+            root.quit()
+            root.destroy()
+            exit()
+        #Create Menu Bar
+        menuBar=Menu(root)
+        root.config(menu=menuBar)
+        #File Menu
+        fileMenu= Menu(menuBar, tearoff=0)
+        fileMenu.add_command(label="New")
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", command=_quit)
+        menuBar.add_cascade(label="File", menu=fileMenu)
+        #Help Menu
+        helpMenu= Menu(menuBar, tearoff=0)
+        helpMenu.add_command(label="About")
+        menuBar.add_cascade(label="Help", menu=helpMenu)
+        def openfile():
+            return filedialog.askopenfilename()
 
-    def displayView(self):
-        self.hello = ["Hallo Welt", "你好，世界", "Hei maailma",
-                      "Hola Mundo", "Привет мир"]
+        GButton_565=tk.Button(root)
+        GButton_565["bg"] = "#111212"
+        ft = tkFont.Font(family='Berlin Sans FB Demi',size=18)
+        GButton_565["font"] = ft
+        GButton_565["fg"] = "#f4f2f2"
+        GButton_565["justify"] = "center"
+        GButton_565["text"] = "Choose program"
+        GButton_565.place(x=270,y=210,width=276,height=202)
+        GButton_565["command"] = openfile
 
-        self.button = QPushButton("Click me!")
-        self.text = QLabel("Hello World")
-        self.text.setAlignment(Qt.AlignCenter)
+        GLabel_800=tk.Label(root)
+        ft = tkFont.Font(family='Berlin Sans FB Demi',size=50)
+        GLabel_800["font"] = ft
+        GLabel_800["fg"] = "#333333"
+        GLabel_800["justify"] = "center"
+        GLabel_800["text"] = "Leakify"
+        GLabel_800.place(x=285,y=60,width=252,height=115)
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
+        GLabel_988=tk.Label(root)
+        ft = tkFont.Font(family='Berlin Sans FB Demi',size=14)
+        GLabel_988["font"] = ft
+        GLabel_988["fg"] = "#333333"
+        GLabel_988["justify"] = "center"
+        GLabel_988["text"] = "Please insert c/cpp file without compilations errors to start"
+        GLabel_988.place(x=110,y=440,width=600,height=30)
 
-    def refreshView(self):
-        pass
-
-def main():
-    app = QApplication(sys.argv)
-
-    widget = GUI()
-    widget.resize(800, 600)
-    widget.show()
-
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
