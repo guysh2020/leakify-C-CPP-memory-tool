4ed0e9eaa363f28fc46cb1dfdf683842b56dd4b9
sivansalzmann <sivansalzmann10@gmail.com>
2021-01-17 00:36:15 +0200

    finish 17/01 00:30

--- view_/GUI.py
+++ view_/GUI.py
@@ -1,16 +1,8 @@
 import os
 import tkinter as tk
 import tkinter.font as tkFont
-from doctest import master
-from tkinter import Menu, END
+from tkinter import Menu, messagebox
 from tkinter import filedialog
-from tkinter import filedialog
-import tkinter.messagebox as box
-import time, threading
-
-
-# from controller.controller import Controller
-
 
 class GUI:
     def __init__(self, controller):
@@ -24,16 +16,12 @@ class GUI:
         exit()
 
     def create(self):
-        # Create Menu Bar
         menuBar = Menu(self.root)
         self.root.config(menu=menuBar)
-        # Help Menu
         helpMenu = Menu(menuBar, tearoff=0)
         helpMenu.add_command(label="About")
         menuBar.add_cascade(label="Help", menu=helpMenu)
-        # setting title
         self.root.title("Leakify")
-        # setting window size
         width = 1200
         height = 600
         screenwidth = self.root.winfo_screenwidth()
@@ -58,7 +46,6 @@ class GUI:
         button_folder["justify"] = "center"
         button_folder["text"] = "choose \n folder"
         button_folder.place(x=70, y=200, width=200, height=100)
-        # button_folder["command"] = self.button_folder_command
 
         label_deatils = tk.Label(self.root)
         ft = tkFont.Font(family='Copperplate Gothic Light', size=20)
@@ -80,30 +67,6 @@ class GUI:
                                "4. Gcc installed on your computer"
         text_deatils.place(x=410, y=250, width=400, height=100)
 
-        c_interactive = tk.Checkbutton(self.root)
-
-        # ft = tkFont.Font(family='Times',size=10)
-        # c_interactive["font"] = ft
-        # c_interactive["fg"] = "#333333"
-        # c_interactive["justify"] = "center"
-        # c_interactive["text"] = "CheckBox"
-        # c_interactive.place(x=930,y=120,width=70,height=25)
-        # c_interactive["offvalue"] = "0"
-        # c_interactive["onvalue"] = "1"
-        # c_interactive.pack()
-        # lbl = tk.Label(self.root)
-        # lbl.pack()
-        #
-        # var = tk.BooleanVar()
-
-        # def update_lbl():
-        #     global var
-        #     if var.get():
-        #         lbl['text'] = str(var.get())
-        #     else:
-        #         print(var.get())
-
-        # tk.Checkbutton(self.root, variable=var, command=update_lbl).pack()
         button_run = tk.Button(self.root, command=self.button_run_command)
 
         button_run["bg"] = "#424d4c"
@@ -128,26 +91,22 @@ class GUI:
         label_log["font"] = ft
         label_log["fg"] = "#333333"
         label_log["justify"] = "center"
-        label_log["text"] = f"Files in directory: {filename}"
-        label_log.place(x=0, y=380, width=1000, height=35)
+        label_log["text"] = f"{filename}"
+        label_log.place(x=0, y=380, width=800, height=35)
 
         Lb1 = tk.Listbox(self.root)
-        Lb1.place(x=0, y=420, width=800, height=228)
+        ft = tkFont.Font(family='Berlin Sans FB', size=12)
+        Lb1["font"] = ft
+        Lb1.place(x=0, y=420, width=900, height=228)
 
         for file in listFiles:
             Lb1.insert(1, file)
 
     def button_run_command(self):
-        label_log = tk.Label(self.root)
-        ft = tkFont.Font(family='Copperplate Gothic Light', size=15)
-        label_log["font"] = ft
-        label_log["fg"] = "#333333"
-        label_log["justify"] = "center"
-        label_log["text"] = "In Procces"
-        label_log.place(x=0, y=380, width=200, height=35)
-
         Lb1 = tk.Listbox(self.root)
-        Lb1.place(x=0, y=420, width=600, height=228)
+        ft = tkFont.Font(family='Berlin Sans FB', size=12)
+        Lb1["font"] = ft
+        Lb1.place(x=0, y=420, width=800, height=228)
 
         errors = self.controller.find_errors()
         for i in errors or []:
@@ -160,17 +119,17 @@ class GUI:
         button_save["fg"] = "#FDFEFE"
         button_save["justify"] = "center"
         button_save["text"] = "Save"
-        button_save.place(x=900, y=420, width=150, height=80)
+        button_save.place(x=930, y=420, width=200, height=100)
         button_save["command"] = self.button_save_command
-
 
 
     def button_save_command(self):
         folder_path = tk.StringVar()
         filename = filedialog.askdirectory()
         folder_path.set(filename)
-
-
         self.controller.save_log(filename)
-
+        messagebox.showinfo("Message", "Log saved successfully!")
+        result = tk.messagebox.askquestion("Message", "Do you want to check another program?", icon='warning')
+        if result == 'no':
+            quit()
 
